0dfee46b21e4781795ae51045829a753acb13522
Lin-Mao <linmaosdu@gmail.com>
2024-10-03 12:20:57 -0700

    Update

--- tracer/analyzer_helper.hpp
+++ tracer/analyzer_helper.hpp
@@ -32,11 +32,11 @@ std::string getCurrentDateTime() {
     // Create a stringstream to format the date and time
     std::stringstream ss;
     ss << (local_time->tm_year + 1900) << "-"  // Year
-       << (local_time->tm_mon + 1) << "-"      // Month (tm_mon is 0-11)
-       << local_time->tm_mday << "_"           // Day of the month
-       << local_time->tm_hour << "-"           // Hour
-       << local_time->tm_min << "-"            // Minute
-       << local_time->tm_sec;                  // Second
+       << std::setw(2) << std::setfill('0') << (local_time->tm_mon + 1) << "-"  // Month (tm_mon is 0-11)
+       << std::setw(2) << std::setfill('0') << local_time->tm_mday << "_"       // Day of the month
+       << std::setw(2) << std::setfill('0') << local_time->tm_hour << "-"       // Hour
+       << std::setw(2) << std::setfill('0') << local_time->tm_min << "-"        // Minute
+       << std::setw(2) << std::setfill('0') << local_time->tm_sec;              // Second
 
     // Convert the stringstream to string and return
     return ss.str();
